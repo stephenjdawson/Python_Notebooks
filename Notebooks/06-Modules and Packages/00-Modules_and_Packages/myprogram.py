29acf28131442a514f52487717ab19adb7b41dca
Stephen Dawson <stephend@hrs.com>
2020-03-07 10:34:00 -0500

    Worked through Packages and Modules Lesson

--- Notebooks/06-Modules and Packages/00-Modules_and_Packages/myprogram.py
+++ Notebooks/06-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -1,6 +1,16 @@
-from MyMainPackage.some_main_script import report_main
+#importing Module
+from mymodule import my_func
+#importing package
+from MyMainPackage import mainscript
+#importing subpackage
 from MyMainPackage.SubPackage import mysubscript
 
-report_main()
+#Can also import specific functions from packages and subpackages if you do not want to pring in whole package. 
+#make sure to have an __init__.py file in your package and subpackage directory so Python knows they are there. 
 
+#Call function from Module
+my_func()
+#Call function in package
+mainscript.report_main()
+#call function in subpackage
 mysubscript.sub_report()
